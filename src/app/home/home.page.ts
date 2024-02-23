41c30dd8ceec257dbfd37fbb69245fa2f63ade17
22ftt1499 <22ftt1499@student.pb.edu.bn>
2024-02-23 14:35:48 +0800

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  memberName!: { fname: string; lname: string };
+  firstName!: string;
+  lastname!: string | undefined;
+
+  constructor(private storage: Storage) {}
+
+  saveUserName(uname: { fname: string; lname: string }) {
+    this.storage.set('username', JSON.stringify(uname));
+  }
+
+  getUserName() {
+    this.storage.get('username').then((data) => {
+      if (data) {
+        this.memberName = JSON.parse(data);
+        this.firstName = this.memberName.fname;
+        this.lastname = this.memberName.lname;
+      }
+    });
+  }
+}
